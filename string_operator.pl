be420e0c32181e2f1af1849839acaf998611bf5a
itstamil <tamilvalavanshanmugam@gmail.com>
2018-08-06 16:24:35 +0530

    string_operation in perl

---
+++ string_operator.pl
@@ -0,0 +1,19 @@
+# . -- > string concadination
+# x followed by n lead to repetaion
+
+#!/usr/bin/perl
+
+#string concadination
+
+$firstname = "tamil valavan";
+$lastname = "shanmugam";
+
+print "the name of employee : ",$firstname.$lastname,"\n";
+
+
+#sting repetaion
+
+$string = tamil ;
+print "length : " ,length("tami::::gfdjgcl\n"), "\n";
+print "the repeat string : ", "$string"x3,"\n";
+
