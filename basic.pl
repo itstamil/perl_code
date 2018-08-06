3db1196a23c6b296ee245a740e00deb1eb259f02
itstamil <tamilvalavanshanmugam@gmail.com>
2018-08-06 16:18:33 +0530

    basic perl script

---
+++ basic.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+
+print"this is basic perl script";
+print"perl supports os commands";
+
+#lite shell script
+#command comment
+
+print `uptime`;
+print "Cpu load balancing : uptime\n";
+system("uptime");
+$a = 10;
+print "Value of a : $a";
