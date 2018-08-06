1bf81dc7cb97731616130fa6ea8663380396fb5f
itstamil <tamilvalavanshanmugam@gmail.com>
2018-08-06 16:22:20 +0530

    nested_if

---
+++ nested_if.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+print "please enter the input :";
+$var =  <>;
+if($var < 10){
+print "the given num is valid num\n";
+if($var == 5){
+print "output obtained\n";
+}
+else{
+print "the number is not equal to 5\n";
+}
+}
+else{
+print "please enter the num smaller than 10\n";
+}
+
