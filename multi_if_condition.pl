3df35501a96ea236267e25117cde28e237ef57a3
itstamil <tamilvalavanshanmugam@gmail.com>
2018-08-06 16:21:53 +0530

    multi_if_condition

---
+++ multi_if_condition.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+print "enter the user name : ";
+chomp($name=<>);
+
+if($name eq "root")
+{
+print "login sucess";
+}
+elsif($name eq "tamil"){
+print "welcome tamil";
+}
+elsif($name eq "sindhuja"){
+print "welcome sindhu";
+}
+elsif($name eq "malathi"){
+print "welcome malathi";
+}
+elsif($name eq "siva"){
+print "welcome siva";
+}
+elsif($name eq "tamil"){
+print "welcome tamil";
+}
+else{
+print "enter the correct name";
+}
